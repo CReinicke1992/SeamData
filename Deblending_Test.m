3ac7d8cfe483304c4fab0b2a9e32f2e221ff5bfb
Christian Reinicke <culeche@gmail.com>
2015-07-21 15:50:26 +0200

    prepared deblending test for random series variation

--- Deblending_Test.m
+++ Deblending_Test.m
@@ -58,26 +58,38 @@ b    = 7;               % Blending factor for a 21 x 51 source grid
 Ne   = Ns/b;            % Number of experiments
 clear fileID Parameters
 
+%% 1.4 Variation of random parameters
+
+% Repeat the computation with different random number series
+% Obtain an average Q
+% Extract maximum Q values
+Nran = 40;  % Number of random number iterations
+
 %% 2 Initiate matrices for quality factors and computation time
 
-quality     = zeros(3,11);          % Number of row:     Number of pattern
+quality     = zeros(3,15,Nran);     % Number of row:     Number of pattern
                                     % Number of columns: Number of shooting windows
 time        = zeros(size(quality));
-incoherency = zeros(size(quality));
+%incoherency = zeros(size(quality));
 
 %% 3 Generate random series for time delays and source permutation
 
+% Initiate random number series
+random_times = zeros(Ne,b-1,Nran);
+random_sources = zeros(Nsi,Nsx,Nran);
+
+for ran = 1:Nran
+
 % Create random series for the firing time delays
-random_times = zeros(Ne,b-1);
 for exp = 1:Ne
-    random_times(exp,:) = rand(b-1,1);
+    random_times(exp,:,ran) = rand(b-1,1);
 end
 
 % Create random series for the source permutation
-random_sources = zeros(Nsi,Nsx);
+
 for in = 1:Nsi
     ind = (in-1)*Nsx + randperm(Nsx);
-    random_sources(in,:) = ind;
+    random_sources(in,:,ran) = ind;
 end
 
 %% 4 Loop over different blending patterns and firing time delays
@@ -98,7 +110,7 @@ for pattern = 1:3
         folder = '3-xline';
     end
     
-    for t_g = 0:10:100 
+    for t_g = 0:10:140 
         
         % tg is supposed to be an even number, to make sure Nt + tg is
         % still an odd number to avoid fft artifacts
@@ -107,6 +119,7 @@ for pattern = 1:3
         end
         
         % Display the iteration number
+        disp(['                      Random iteration: ',sprintf('%d / %d',ran,Nran)]);
         disp(['                               Pattern: (',num2str(pattern), '/3), (',folder,')']);
         disp(['                                   t_g: ',num2str(t_g)]);
         disp(['Duration of the previous iteration (s): ',num2str(time_deblending)]);
@@ -120,9 +133,10 @@ for pattern = 1:3
         path = strcat('Data',path_for_blend_deblend);
         
         % Create a 2d blending matrix
-        g = gxin(t_g,Ns,Nsx,b,pattern,random_times,random_sources);
-        mu = incoherency_dia(g,Nt);
+        g = gxin(t_g,Ns,Nsx,b,pattern,squeeze(random_times(:,:,ran)),...
+                        squeeze(random_sources(:,:,ran)));
         %save(strcat(path,'g_matrix.mat'),'g')
+        %mu = incoherency_dia(g,Nt);
         
         % Blend & deblend the data. Measure the computation time.
         bl = tic;
@@ -130,47 +144,49 @@ for pattern = 1:3
         time_deblending = toc(bl);
      
         % Load quality factor
-        fileID = strcat(path,'Q.mat');
-        Quality = load(fileID);
-        Q = Quality.Q; clear fileID Quality
+%         fileID = strcat(path,'Q.mat');
+%         Quality = load(fileID);
+%         Q = Quality.Q; clear fileID Quality
         
         % Write a result file
-        fid = fopen(strcat(path,'Results.txt'),'w');
-        
-        fprintf(fid,'Acquisition Set Up \n');
-        fprintf(fid,'Parameter file: \t\tData/Parameters_red.mat \n');
-        fprintf(fid,'Blending factor: \t\t%d \n',b);
-        fprintf(fid,'Shooting window (seconds): \t%f \n',t_g*dt);
-        fprintf(fid,strcat('Blending pattern: \t\t',folder,'\n\n'));
-        
-        fprintf(fid,'Deblending quality: \t\t%f \n',Q(1));
-        fprintf(fid,'Computing time (seconds): \t%f \n\n',time_deblending);
-        
-        %fprintf(fid,'Incoherency: \t\t %f \n',in);
-        %fprintf(fid,'Computing time (seconds): \t%f \n',time_incoherency);
-        
-        fclose(fid); clear fid
+        % Uncomment to write a result fil
+%         fid = fopen(strcat(path,'Results.txt'),'w');
+%         
+%         fprintf(fid,'Acquisition Set Up \n');
+%         fprintf(fid,'Parameter file: \t\tData/Parameters_red.mat \n');
+%         fprintf(fid,'Blending factor: \t\t%d \n',b);
+%         fprintf(fid,'Shooting window (seconds): \t%f \n',t_g*dt);
+%         fprintf(fid,strcat('Blending pattern: \t\t',folder,'\n\n'));
+%         
+%         fprintf(fid,'Deblending quality: \t\t%f \n',Q(1));
+%         fprintf(fid,'Computing time (seconds): \t%f \n\n',time_deblending);
+%         
+%         fprintf(fid,'Incoherency: \t\t %f \n',mu);
+%         fprintf(fid,'Computing time (seconds): \t%f \n',t_in);
+%         
+%         fclose(fid); clear fid
         
         index = floor(t_g/10)+1;      
-        quality(pattern,index)     = Q(1);
-        time(pattern,index)        = time_deblending;
-        %incoherency_matrix(pattern,index,ran) = in;
+        quality(pattern,index,ran)     = Q(1);
+        time(pattern,index,ran)        = time_deblending;
+        %incoherency(pattern,index,ran) = mu;
         
     end
 end
 
+end
 total_time = toc(total);
 
 
 %% 5 Save results 
 
-save('Data/ParameterTest/Total_Elapsed_Time','total_time')
+save('Data/ParameterTest/ran_Total_Elapsed_Time','total_time')
 
 % Save measured parameters
-save('Data/ParameterTest/quality','quality')
-save('Data/ParameterTest/time','time')
-%save('Data/ParameterTest/incoherency','incoherency_matrix')
+save('Data/ParameterTest/ran_quality','quality')
+save('Data/ParameterTest/ran_time','time')
+%save('Data/ParameterTest/incoherency','incoherency')
 
 % Save random series
-save('Data/ParameterTest/rand_times','random_times')
-save('Data/ParameterTest/rand_sources','random_sources')
+save('Data/ParameterTest/ran_rand_times','random_times')
+save('Data/ParameterTest/ran_rand_sources','random_sources')
